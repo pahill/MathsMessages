96d483a7f4b51af33b55bac65a752d068e6f25af
Pamela Hill <pamela.a.hill@gmail.com>
2024-02-08 11:39:52 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
       .library(name: "shared", targets: ["shared"])
    ],
    targets: [
-      .binaryTarget(name: "shared", url: "https://github.com/pahill/MathsMessages/shared.xcframework.zip")
-   ],
+      .binaryTarget(name: "shared", url: "https://github.com/pahill/MathsMessages/shared.xcframework.zip", checksum:"a425087ac52412ba84b252b87aaf06442d9706cbb119e299880d48186b0873a1")
+   ]
 )
